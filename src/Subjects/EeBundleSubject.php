950cc325ae3804d7f9415e9560710b3971b358ee
Tim Wagner <t.wagner@techdivision.com>
2019-06-06 10:23:37 +0200

    Refactoring

--- src/Subjects/EeBundleSubject.php
+++ src/Subjects/EeBundleSubject.php
@@ -57,7 +57,7 @@ class EeBundleSubject extends BundleSubject
         $registryProcessor = $this->getRegistryProcessor();
 
         // load the status of the actual import process
-        $status = $registryProcessor->getAttribute($serial);
+        $status = $registryProcessor->getAttribute(RegistryKeys::STATUS);
 
         // load the attribute set we've prepared intially
         $this->skuRowIdMapping = $status[RegistryKeys::SKU_ROW_ID_MAPPING];
